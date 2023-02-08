1beb1d263b7ad3af6735e77cbbe540dadab52e6b
beatrizslima <bialima.ricarte@gmail.com>
2023-02-07 21:23:13 -0300

    Adapter class

--- app/src/main/java/br/com/etecia/newproject/MainActivity.java
+++ app/src/main/java/br/com/etecia/newproject/MainActivity.java
@@ -3,12 +3,49 @@ package br.com.etecia.newproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listId = findViewById(R.id.listId);
+
+        MyAdapter adapter = new MyAdapter();
+        listId.setAdapter(adapter);
+
     }
+
+
+    public class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
+
 }
